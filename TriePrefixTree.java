d74da4c745fe09ca48c78f6721e52199253f3f8c
Chaitanya7725 <chaitanya7725@gmail.com>
2025-02-08 18:21:48 -0500

    Adding Trie Prefix Tree

---
+++ TriePrefixTree.java
@@ -0,0 +1,65 @@
+// TC: O(l) as the word is traversed with length l
+// SC: O(l) as the l length of word Tries are used. The children array is constant of 26 spaces 
+
+// Custom class for creating the user-defined DS
+class TrieNode {
+    TrieNode[] children;
+    boolean isEnd;
+
+    TrieNode() {
+        children = new TrieNode[26];
+        isEnd = false;
+    }
+}
+
+// Handles all the operation for tries
+class Trie {
+
+    TrieNode root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null)
+                curr.children[c - 'a'] = new TrieNode();
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for (char c : word.toCharArray()) {
+            if (curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for (char c : prefix.toCharArray()) {
+            if (curr.children[c - 'a'] == null)
+                return false;
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+public class TriePrefixTree {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("apple");
+        System.out.println(trie.search("apple")); // true
+        System.out.println(trie.search("app")); // false
+        System.out.println(trie.startsWith("app")); // true
+        trie.insert("app");
+        System.out.println(trie.search("app")); // true
+    }
+}
